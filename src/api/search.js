8b4eab60afd8c9397e31069655bf09d5c65109ab
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-01-23 12:57:30 -0300

    List only the required fields.

--- src/api/search.js
+++ src/api/search.js
@@ -3,9 +3,28 @@ const ytsr = require('ytsr');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const searchResults = await ytsr('rickroll');
-    res.json(searchResults);
+router.get('/', async (req, res, next) => {
+    try {        
+        const searchResults = await ytsr('rickroll, this week');
+        const simplifiedSearchResults = searchResults.items.map(o => ({
+            title: o.title,
+            url: o.url,
+            thumbnail: o.bestThumbnail,
+            author: {
+                name: o.author && o.author.name || '**Unknown**',
+                avatar: o.author && o.author.bestAvatar
+            },            
+            description: o.description,
+            views: o.views,
+            duration: o.duration,
+            uploadedAt: o.uploadedAt,
+            original: o
+        }));
+        res.json(simplifiedSearchResults);
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
 });
 
 module.exports = router;
