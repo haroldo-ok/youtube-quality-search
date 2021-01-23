b610c98bafa612bd0c6a28368e2f3ad7cc330f04
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-01-23 13:13:06 -0300

    Calculate number of days ago.

--- src/api/search.js
+++ src/api/search.js
@@ -6,20 +6,25 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
     try {        
         const searchResults = await ytsr('rickroll, this week');
-        const simplifiedSearchResults = searchResults.items.map(o => ({
-            title: o.title,
-            url: o.url,
-            thumbnail: o.bestThumbnail,
-            author: {
-                name: o.author && o.author.name || '**Unknown**',
-                avatar: o.author && o.author.bestAvatar
-            },            
-            description: o.description,
-            views: o.views,
-            duration: o.duration,
-            uploadedAt: o.uploadedAt,
-            original: o
-        }));
+        const simplifiedSearchResults = searchResults.items
+        .filter(o => (o.uploadedAt || '').indexOf('day') !== -1)
+        .map(o => {
+            return {
+                title: o.title,
+                url: o.url,
+                thumbnail: o.bestThumbnail,
+                author: {
+                    name: o.author && o.author.name || '**Unknown**',
+                    avatar: o.author && o.author.bestAvatar
+                },            
+                description: o.description,
+                views: o.views,
+                duration: o.duration,
+                uploadedAt: o.uploadedAt,
+                daysAgo: parseInt(o.uploadedAt),
+                original: o
+            };
+        });
         res.json(simplifiedSearchResults);
     } catch (e) {
         console.error(e);
